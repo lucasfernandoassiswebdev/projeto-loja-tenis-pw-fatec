38da0ffbec310fce1abc602b3edbdd0fdd693686
lucasaassis@outlook.com <lucasaassis@outlook.com>
2018-09-25 22:05:55 -0300

    refatorando e encapsulando tratamento de exceptions de serviços

--- services/MarcaService.js
+++ services/MarcaService.js
@@ -1,8 +1,9 @@
 const marcaRepository = require("../repositories/MarcaRepository");
+const serviceExceptions = require('./exceptions/serviceExceptions');
 
 exports.get = async (req, res) => {
     return await marcaRepository.find(function (error, marcas) {
-        treatError(error, 500, 'Algo deu errado ao buscar as marcas cadastradas', function (goWrong, error_code, error_message) {
+        serviceExceptions.treatError(error, 500, 'Algo deu errado ao buscar as marcas cadastradas', function (goWrong, error_code, error_message) {
             if (goWrong)
                 return res.status(error_code).send({ message: error_message });
 
@@ -14,12 +15,12 @@ exports.get = async (req, res) => {
 exports.getById = async (req, res) => {
     return await marcaRepository.findById(req.params.id, function (error, marca) {
         if (marca == null)
-            treatError(true, 404, 'Marca buscada não existe', function (goWrong, error_code, error_message) {
+            serviceExceptions.treatError(true, 404, 'Marca buscada não existe', function (goWrong, error_code, error_message) {
                 if (goWrong)
                     return res.status(error_code).send({ message: error_message });
             });
         else
-            treatError(error, 500, 'Algo deu errado ao buscar a marca', function (goWrong, error_code, error_message) {
+            serviceExceptions.treatError(error, 500, 'Algo deu errado ao buscar a marca', function (goWrong, error_code, error_message) {
                 if (goWrong)
                     return res.status(error_code).send({ message: error_message });
 
@@ -29,16 +30,16 @@ exports.getById = async (req, res) => {
 }
 
 exports.post = async (req, res) => {
-    validateAndSave(req, false, function (error, exists) {
-        treatError(error, 500, 'Algo deu errado ao salvar a marca', function () {
+    serviceExceptions.validateAndSave(req, false, marcaRepository, function (error, exists) {
+        serviceExceptions.treatError(error, 500, 'Algo deu errado ao salvar a marca', function () {
             if (exists)
-                treatError(true, 500, 'Já existe uma marca com este nome cadastrada!', function (goWrong, error_code, error_message) {
+                serviceExceptions.treatError(true, 500, 'Já existe uma marca com este nome cadastrada!', function (goWrong, error_code, error_message) {
                     if (goWrong)
                         return res.status(error_code).send({ message: error_message });
                 });
             else
                 marcaRepository.save(req.body, function (error) {
-                    treatError(error, 500, 'Algo deu errado ao salvar a marca', function (goWrong, error_code, error_message) {
+                    serviceExceptions.treatError(error, 500, 'Algo deu errado ao salvar a marca', function (goWrong, error_code, error_message) {
                         if (goWrong)
                             return res.status(error_code).send({ message: error_message });
 
@@ -50,18 +51,18 @@ exports.post = async (req, res) => {
 };
 
 exports.put = async (req, res) => {
-    return await validateAndSave(req, true, function (error, exists) {
-        treatError(error, 500, 'Algo deu errado ao atualizar a marca', function (goWrong, error_code, error_message) {
+    return await serviceExceptions.validateAndSave(req, true, marcaRepository, function (error, exists) {
+        serviceExceptions.treatError(error, 500, 'Algo deu errado ao atualizar a marca', function (goWrong, error_code, error_message) {
             if (goWrong)
                 return res.status(error_code).send({ message: error_message });
             else if (!exists)
-                treatError(true, 404, 'Marca que está tentando ser editada não existe!', function (goWrong, error_code, error_message) {
+                serviceExceptions.treatError(true, 404, 'Marca que está tentando ser editada não existe!', function (goWrong, error_code, error_message) {
                     if (goWrong)
                         return res.status(error_code).send({ message: error_message });
                 });
             else
                 marcaRepository.update(req.params.id, req.body, function (error) {
-                    treatError(error, 500, 'Algo deu errado ao editar a marca', function (goWrong, error_code, error_message) {
+                    serviceExceptions.treatError(error, 500, 'Algo deu errado ao editar a marca', function (goWrong, error_code, error_message) {
                         if (goWrong)
                             return res.status(error_code).send({ message: error_message });
 
@@ -73,18 +74,18 @@ exports.put = async (req, res) => {
 }
 
 exports.delete = async (req, res) => {
-    return await validateAndSave(req, true, function (error, exists) {
-        treatError(error, 500, 'Algo deu errado ao excluir a marca', function (goWrong, error_code, error_message) {
+    return await serviceExceptions.validateAndSave(req, true, marcaRepository, function (error, exists) {
+        serviceExceptions.treatError(error, 500, 'Algo deu errado ao excluir a marca', function (goWrong, error_code, error_message) {
             if (goWrong)
                 return res.status(error_code).send({ message: error_message });
             else if (!exists)
-                treatError(true, 404, 'Marca não existe!', function (goWrong, error_code, error_message) {
+                serviceExceptions.treatError(true, 404, 'Marca não existe!', function (goWrong, error_code, error_message) {
                     if (goWrong)
                         return res.status(error_code).send({ message: error_message });
                 });
             else
                 marcaRepository.delete(req.params.id, function (error) {
-                    treatError(error, 500, 'Algo deu errado ao excluir a marca', function (goWrong, error_code, error_message) {
+                    serviceExceptions.treatError(error, 500, 'Algo deu errado ao excluir a marca', function (goWrong, error_code, error_message) {
                         if (goWrong)
                             return res.status(error_code).send({ message: error_message });
 
@@ -94,27 +95,3 @@ exports.delete = async (req, res) => {
         });
     });
 };
-
-async function validateAndSave(req, update, callback) {
-    if (update) {
-        await marcaRepository.findById(req.params.id, function (error, marca) {
-            if (typeof callback === "function")
-                callback(error, marca != null);
-        });
-    } else {
-        await marcaRepository.findByName(req.body.nome, function (error, marca) {
-            if (typeof callback === "function")
-                callback(error, marca != null);
-        });
-    }
-}
-
-function treatError(error, code, message, callback) {
-    var error_code = (code != null && code != undefined) ? code : 500;
-    var error_message = (message != undefined && message != null && message != '') ? message : 'Ocorreu um erro ao processar sua requisição';
-
-    if (error && callback != undefined && typeof callback === "function")
-        callback(true, error_code, error_message);
-    else if (callback != undefined && typeof callback === "function")
-        callback(false, null, null);
-}
---
+++ services/exceptions/serviceExceptions.js
@@ -0,0 +1,23 @@
+exports.validateAndSave = async (req, byId, repository, callback) => {
+    if (byId) {
+        await repository.findById(req.params.id, function (error, marca) {
+            if (typeof callback === "function")
+                callback(error, marca != null);
+        });
+    } else {
+        await repository.findByName(req.body.nome, function (error, marca) {
+            if (typeof callback === "function")
+                callback(error, marca != null);
+        });
+    }
+};
+
+exports.treatError = (error, code, message, callback) => {
+    var error_code = (code != null && code != undefined) ? code : 500;
+    var error_message = (message != undefined && message != null && message != '') ? message : 'Ocorreu um erro ao processar sua requisição';
+
+    if (error && callback != undefined && typeof callback === "function")
+        callback(true, error_code, error_message);
+    else if (callback != undefined && typeof callback === "function")
+        callback(false, null, null);
+};
